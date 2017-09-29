b6128a496ebae6c694d0277acc5b6e39c339034c
MANJUNATH <MANJUNATH@SAMSUNG.Manjunath>
2017-09-29 16:22:21 +0530

    committing assignment 5.2

---
+++ src/assignment5_2/Figure.java
@@ -0,0 +1,13 @@
+package assignment5_2;
+
+//Abstract Class with the given properties of the Figure and the abstract methods to be implemented in the subclasses
+public abstract class Figure {
+//	Dimension variables
+double dim1;
+double dim2;
+double dim3;
+
+//	Abstract Methods
+abstract void findArea();
+abstract void findPerimeter();
+}
